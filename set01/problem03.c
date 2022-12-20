c835a8459e900274bcc4db5c60eb7dc08015a9d0
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2022-12-20 13:43:00 +0000

    sloved

--- set01/problem03.c
+++ set01/problem03.c
@@ -2,16 +2,16 @@
 int main(void)
 {
   int a,b,sum;
-  printf("enter the first number\n");
+  printf("enter the first number, \d");
   scanf("%d",&a);
-  printf("enter the second number\n");
-  scanf("%d",&b);
+  print("enter the second number,\d");
+  scanf("%b",&b);
   sum=sum_func(a,b);
   printf("the sum of the 2 number is %d",sum);
+  
 }
-sum_func(int a,int b)
-{
-  int sum;
+sum_func(int a, int b)
+{int sum;
   sum=a+b;
-return sum;
-}
+  return sum;
+  }

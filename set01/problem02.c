5e871dbdcdbd4ad6962dadf7096322017d57cb9d
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2022-12-23 09:26:10 +0000

    sloved

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-main()
+ int main(void)
  {
   int a,b,add;
 printf("enter two number");
   scanf("%d%d",&a,&b);
   add=a+b;
     printf("add the numbers=%d",add);
+   retrun 0;
   
 }

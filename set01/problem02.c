4be147e7d20488965035769d0f17cec4736a19e1
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2023-01-02 15:15:07 +0000

    sloved

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
- int main(void)
- {
-  int a,b,add;
-printf("enter two number");
-  scanf("%d%d",&a,&b);
-  add=a+b;
-    printf("add the numbers=%d",add);
-   retrun 0;
-  
+int main(void)
+{
+  int=
 }

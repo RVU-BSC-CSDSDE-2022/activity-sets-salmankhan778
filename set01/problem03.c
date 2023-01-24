efd36d12feeb589f33b1ca26aee050563d95f6d4
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2023-01-24 16:56:25 +0000

    sloved

--- set01/problem03.c
+++ set01/problem03.c
@@ -9,22 +9,22 @@ int input()
   scanf("%d",&a);
   return a;
 }
-int add(int a, int b)
+int add( int a, int b)
 {
- int sum;
+  int sum;
   sum=a+b;
   return sum;
 }
 void output(int a, int b, int sum)
 {
-  printf("sum of the %d and %d is %d\n", a,b,sum);
-}
+  printf("addtion of the two number %d and %d id %d\n",a,b,sum);
+  }
 int main()
 {
   int a,b,sum;
   a =input();
   b =input();
-  sum= add(a,b);
+  sum =add(a,b);
   output(a,b,sum);
   return 0;
-}
+  }

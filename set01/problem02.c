0d85bd8b3863c72f42cf20311c2bdf29e5f1d9ec
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2023-01-24 16:44:20 +0000

    sloved

--- set01/problem02.c
+++ set01/problem02.c
@@ -4,7 +4,7 @@ int main()
   int a,b,add;
   printf("enter the two numbers");
   scanf("%d%d",&a,&b);
-  add=a+b;
-  printf("addition of the two numbers %d and %d is %d ",a,b,add);
+    add=a+b;
+  printf("addition of the  two %d and %d is %d\n", a,b,add);
   return 0;
-    }
+}

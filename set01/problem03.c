767285c33c6c9e0315a29f4642d9903748c16709
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2023-02-04 09:11:30 +0000

    sloved

--- set01/problem03.c
+++ set01/problem03.c
@@ -4,27 +4,28 @@ int add(int a, int b);
 void output(int a, int b, int sum);
 int input()
 {
-int a;
-printf("enter the number");
-scanf("%d",&a);
-return a;
+  int a;
+  printf("enter the number");
+  scanf("%d",&a);
+  return a;
 }
 int add(int a, int b)
 {
-int sum;
-  sum=a+b;
-return sum;
+  int sum
+  sum = a,b;
+  return sum;
 }
 void output(int a, int b, int sum)
 {
-printf("addtion of the two numbers %d and %d is %d\n",a,b,sum);
-}
+  printf("addition of the two numbers %d and %d is %d\n",a,b,sum);
+  }
 int main()
 {
-int a,b, sum;
-a= input();
-b=input();
-sum = add(a,b);
-output(a,b,sum);
-return 0;
+  int a,b,sum;
+  a= input();
+  b = input();
+  sum=add(a,b);
+  output(a,b,&sum);
+  return 0;
+  
 }

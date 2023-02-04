bf8367181afd2d400368c95495648f7f5ef63dd6
Salman BSc <114485094+salmankhan778@users.noreply.github.com>
2023-02-04 14:55:02 +0000

    sloved

--- set01/problem07.c
+++ set01/problem07.c
@@ -5,30 +5,31 @@ void output(int n, int sum);
 int input()
 {
   int n;
-  printf("enter the number:");
+  printf("enter the number");
   scanf("%d",&n);
   return n;
-}
-int sum_n_nos( int n)
+  }
+int sum_n_nos(int n)
 {
   int sum=0,i;
   i=0;
   while(i<=n)
-    {
-      i++;
-      sum=sum+i;
-    }
-  return(sum);
+  {
+    i++;
+    sum=sum+i;
+    
+  }
+  return (sum);
 }
 void output(int n, int sum)
 {
-  printf("the sum of first %d natural numbers is %d",n,sum);
-  }
+  printf("the sum of the first  %d natural number is %d",n,sum);
+}
 int main()
 {
   int n,i,sum;
   n=input();
   sum=sum_n_nos(n);
   output(n,sum);
-  return (0);
-} 
+  return 0;
+    }
